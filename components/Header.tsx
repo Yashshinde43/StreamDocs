2042367fe7aac2151249ef749161b11bf974219c
Yash Shinde <111728261+Yashshinde43@users.noreply.github.com>
2024-07-30 14:43:18 +0530

    first commit

---
+++ components/Header.tsx
@@ -0,0 +1,23 @@
+import { cn } from '@/lib/utils'
+import Image from 'next/image'
+import Link from 'next/link'
+
+const Header = ({ children, className }: HeaderProps) => {
+  return (
+    <div className={cn("header", className)}>
+      <Link href='/' className="md:flex-1">
+        <h1 className='text-2xl font-semibold ml-2'>StreamDocs</h1>
+        <Image 
+          src="/assets/icons/logo-icon.svg"
+          alt="Logo"
+          width={32}
+          height={32}
+          className="mr-2 md:hidden"
+        />
+      </Link>
+      {children}
+    </div>
+  )
+}
+
+export default Header
